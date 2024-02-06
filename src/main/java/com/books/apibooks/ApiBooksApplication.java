bae3aa6682c851913ad65e90417574a4523415ba
matheuspereira00 <matheusinhofcb12@gmail.com>
2024-02-06 10:11:50 -0300

    first

---
+++ src/main/java/com/books/apibooks/ApiBooksApplication.java
@@ -0,0 +1,13 @@
+package com.books.apibooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/books/apibooks/ApiBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.books.apibooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
