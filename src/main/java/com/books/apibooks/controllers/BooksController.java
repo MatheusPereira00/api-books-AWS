b99a87d83c720f01cbac877c88e7544010e7aac8
matheuspereira00 <matheusinhofcb12@gmail.com>
2024-02-06 13:06:04 -0300

    feat: add controller

--- src/main/java/com/books/apibooks/controllers/BooksController.java
+++ src/main/java/com/books/apibooks/controllers/BooksController.java
@@ -1,11 +1,28 @@
 package com.books.apibooks.controllers;
 
+import com.books.apibooks.domain.Book;
+import com.books.apibooks.feign.BookClient;
+import com.books.apibooks.service.BookService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/books")
 public class BooksController {
 
+    private final BookService bookService;
 
+    public BooksController(BookService bookService){
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public ResponseEntity <List<Book>> getBooks(){
+        List<Book> Books = this.bookService.getBooks();
+        return ResponseEntity.ok().body(Books);
+    }
 }
